30a415d73f97d5d4423b178fb53cf5f70113dddb
JohnCorby <williamforgames6@gmail.com>
2024-10-31 21:05:22 -0700

    do flight shuttle stuff in unity

--- OWJam4ModProject/ShuttleFlightController.cs
+++ OWJam4ModProject/ShuttleFlightController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace OWJam4ModProject
 {
+    /// <summary>
+    /// handles takeoff, landing, and flighting orbit around the planet
+    /// </summary>
     internal class ShuttleFlightController : MonoBehaviour
     {
         [Tooltip("The name of the transform to target")]
@@ -12,8 +15,6 @@ namespace OWJam4ModProject
         [SerializeField] float orbitRadius;
         [Tooltip("How fast the shuttle moves")]
         [SerializeField] float flightSpeed;
-        [Tooltip("How fast the shuttle rotates to face towards the planet as it flies there")]
-        [SerializeField] float flightAlignmentSpeed;
         [Tooltip("The light sensor that activates flight")]
         [SerializeField] SingleLightSensor lightSensor;
         [Tooltip("The shuttle's OWRigidbody")]
@@ -26,10 +27,6 @@ namespace OWJam4ModProject
             lightSensor.OnDetectLight += StartFlight;
 
             landingTarget = SearchUtilities.Find(landingTargetName).transform;
-            // terrible. please do this in unity
-            var b = landingTarget.gameObject.AddComponent<OWRigidbody>();
-            b.MakeKinematic();
-            b.EnableKinematicSimulation();
         }
 
         void OnDestroy()
@@ -52,13 +49,9 @@ namespace OWJam4ModProject
             body.SetVelocity(velocity);
 
             // start aligning with body
-            // should probably put this on the prefab lol
-            var align = body.gameObject.AddComponent<AlignWithTargetBody>();
-            align.SetLocalAlignmentAxis(Vector3.up);
-            align.SetUsePhysicsToRotate(true);
+            var align = body.GetComponent<AlignWithTargetBody>();
             align.SetTargetBody(landingTarget.GetAttachedOWRigidbody());
-            align._interpolationMode = AlignWithDirection.InterpolationMode.Linear; // change the mode to make it nicer. idk what ship uses for landing cam
-            align._interpolationRate = 10;
+            align.enabled = true;
 
             yield return null;
         }
