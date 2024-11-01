ce01900bd548b91bc38dce6c74b780383e6b8655
mcdon546 <48959058+2walker2@users.noreply.github.com>
2024-11-01 16:23:15 -0400

    Added shuttle light

---
+++ OWJam4ModProject/ShuttleLightController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace OWJam4ModProject
+{
+    internal class ShuttleLightController : MonoBehaviour
+    {
+        [Tooltip("The light sensor used to turn on the light")]
+        [SerializeField] SingleLightSensor lightSensor;
+        [Tooltip("The objects to toggle based on whether the light is on")]
+        [SerializeField] GameObject[] objectsToToggle;
+        [Tooltip("The audio to play when the light is activated")]
+        [SerializeField] OWAudioSource lightActivatedAudio;
+        [Tooltip("The audio to play when the light is deactivated")]
+        [SerializeField] OWAudioSource lightDeactivatedAudio;
+
+        void Start ()
+        {
+            lightSensor.OnDetectLight += TurnOnLight;
+            lightSensor.OnDetectDarkness += TurnOffLight;
+        }
+
+        void OnDestroy()
+        {
+            lightSensor.OnDetectLight -= TurnOnLight;
+            lightSensor.OnDetectDarkness -= TurnOffLight;
+        }
+
+        void TurnOnLight()
+        {
+            foreach (GameObject g in objectsToToggle)
+                g.SetActive(true);
+
+            lightActivatedAudio.PlayOneShot();
+        }
+
+        void TurnOffLight()
+        {
+            foreach (GameObject g in objectsToToggle)
+                g.SetActive(false);
+
+            lightDeactivatedAudio.PlayOneShot();
+        }
+    }
+}
