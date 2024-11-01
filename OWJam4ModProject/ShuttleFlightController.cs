364d53c6aabe01801850b6783714f2f3ecc4a08f
JohnCorby <williamforgames6@gmail.com>
2024-10-31 22:40:54 -0700

    (bad) shuttle flight

--- OWJam4ModProject/ShuttleFlightController.cs
+++ OWJam4ModProject/ShuttleFlightController.cs
@@ -22,6 +22,12 @@ namespace OWJam4ModProject
         [Tooltip("The shuttle's OWRigidbody")]
         [SerializeField] OWRigidbody body;
 
+        [Header("Orbit sensors")]
+        public SingleLightSensor ForwardSensor;
+        public SingleLightSensor BackSensor;
+        public SingleLightSensor LeftSensor;
+        public SingleLightSensor RightSensor;
+
         Transform landingTarget;
 
         void Start()
@@ -41,8 +47,10 @@ namespace OWJam4ModProject
             OWJam4ModProject.instance.ModHelper.Console.WriteLine("Starting shuttle flight", OWML.Common.MessageType.Success);
 
             // Don't take off if the takeoff puzzle isn't solved
-            if (!takeoffController.CanTakeOff())
+            if (false && !takeoffController.CanTakeOff())
                 return;
+
+            lightSensor.OnDetectLight -= StartFlight;
 
             StartCoroutine(FlyToPlanet());
         }
@@ -75,6 +83,17 @@ namespace OWJam4ModProject
         {
             while (true)
             {
+                var direction = Vector3.zero;
+                if (ForwardSensor.IsIlluminated()) direction += Vector3.forward;
+                if (BackSensor.IsIlluminated()) direction += Vector3.back;
+                if (LeftSensor.IsIlluminated()) direction += Vector3.left;
+                if (RightSensor.IsIlluminated()) direction += Vector3.right;
+                direction = transform.TransformDirection(direction.normalized * 10/*bleh*/);
+
+                body.SetVelocity(direction);
+                // TODO: make it stay in the orbit radius
+                // TODO: acceleration and DRAG
+
                 yield return null;
             }
         }
