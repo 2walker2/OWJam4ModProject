2eeb5ac2a36b221518ba3d063bd61699af7f2076
JohnCorby <williamforgames6@gmail.com>
2024-11-03 14:23:29 -0800

    fix dithering animator

--- OWJam4ModProject/DitheringAnimatorPatches.cs
+++ OWJam4ModProject/DitheringAnimatorPatches.cs
@@ -1,14 +1,31 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace OWJam4ModProject;
 
-// some things dont have dither shader variable on them, so we have to do this
 [HarmonyPatch(typeof(DitheringAnimator))]
 public static class DitheringAnimatorPatches
 {
+	[HarmonyPostfix, HarmonyPatch(nameof(DitheringAnimator.Awake))]
+	private static void DitheringAnimator_Awake(DitheringAnimator __instance)
+	{
+		// CHANGED: include inactive
+		Renderer[] componentsInChildren = __instance.GetComponentsInChildren<Renderer>(true);
+		__instance._renderers = new OWRenderer[componentsInChildren.Length];
+		for (int i = 0; i < __instance._renderers.Length; i++)
+		{
+			__instance._renderers[i] = componentsInChildren[i].GetComponent<OWRenderer>();
+			if (__instance._renderers[i] == null)
+			{
+				__instance._renderers[i] = componentsInChildren[i].gameObject.AddComponent<OWRenderer>();
+			}
+		}
+	}
+
 	[HarmonyPostfix, HarmonyPatch(nameof(DitheringAnimator.UpdateDithering))]
 	private static void DitheringAnimator_UpdateDithering(DitheringAnimator __instance)
 	{
+		// some things dont have dither shader variable on them, so we have to do this
 		foreach (var renderer in __instance._renderers)
 		{
 			if (!renderer) continue;
