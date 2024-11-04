c585969ce840db56221b78542c4b240fc474b23e
JohnCorby <williamforgames6@gmail.com>
2024-11-04 11:55:39 -0800

    admin artifact interactable

---
+++ OWJam4ModProject/AdminArtifactInteractable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace OWJam4ModProject;
+
+public class AdminArtifactInteractable : MonoBehaviour
+{
+	public InteractReceiver InteractReceiver;
+
+	private void Awake()
+	{
+		InteractReceiver.ChangePrompt("Elevate Privileges");
+		InteractReceiver.OnPressInteract += OnPressInteract;
+	}
+
+	private void OnDestroy()
+	{
+		InteractReceiver.OnPressInteract -= OnPressInteract;
+	}
+
+	private void OnPressInteract()
+	{
+		gameObject.SetActive(false);
+		AdminArtifact.AttachToPlayerLantern();
+	}
+}
