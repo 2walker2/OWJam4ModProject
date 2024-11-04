4490fab781c56fb8742f8661d860537d5c36577e
JohnCorby <williamforgames6@gmail.com>
2024-11-03 21:33:49 -0800

    morse beep

---
+++ OWJam4ModProject/MorseCodeBeep.cs
@@ -0,0 +1,36 @@
+﻿using NewHorizons.Utility.OWML;
+using UnityEngine;
+
+namespace OWJam4ModProject;
+
+public class MorseCodeBeep : MonoBehaviour
+{
+	public SingleLightSensor LightSensor;
+	public OWAudioSource AudioSource;
+
+	private void Start()
+	{
+		LightSensor.OnDetectLight += OnDetectLight;
+		LightSensor.OnDetectDarkness += OnDetectDarkness;
+
+		// owaudiosource is being bad so dont use it
+		AudioSource._audioSource.volume = 0;
+		AudioSource._audioSource.Play();
+	}
+
+	void OnDestroy()
+	{
+		LightSensor.OnDetectLight -= OnDetectLight;
+		LightSensor.OnDetectDarkness -= OnDetectDarkness;
+	}
+
+	private void OnDetectLight()
+	{
+		AudioSource._audioSource.volume = .5f;
+	}
+
+	private void OnDetectDarkness()
+	{
+		AudioSource._audioSource.volume = 0;
+	}
+}
