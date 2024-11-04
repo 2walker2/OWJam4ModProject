10387f7822d786206ede28f644722370dc8ce900
mcdon546 <48959058+2walker2@users.noreply.github.com>
2024-11-04 14:44:24 -0500

    Time pauses on dreamworld

---
+++ OWJam4ModProject/FreezeTimeVolume.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace OWJam4ModProject
+{
+    internal class FreezeTimeVolume : MonoBehaviour
+    {
+        [Tooltip("The trigger volume to freeze time within")]
+        [SerializeField] OWTriggerVolume triggerVolume;
+
+        bool inTrigger = false;
+        float secondsRemainingWhenEntered;
+
+        void Start ()
+        {
+            triggerVolume.OnEntry += EnteredTrigger;
+            triggerVolume.OnExit += ExitedTrigger;
+        }
+
+        void Update ()
+        {
+            if (inTrigger)
+            {
+                TimeLoop.SetSecondsRemaining(secondsRemainingWhenEntered);
+            }
+        }
+
+        private void EnteredTrigger(GameObject hitObj)
+        {
+            if (hitObj.CompareTag("PlayerDetector"))
+            {
+                inTrigger = true;
+                secondsRemainingWhenEntered = TimeLoop.GetSecondsRemaining();
+            }
+        }
+
+        private void ExitedTrigger(GameObject hitObj)
+        {
+            if (hitObj.CompareTag("PlayerDetector"))
+            {
+                inTrigger = false;
+            }
+        }
+    }
+}
