070b15932fbb70301063562db86508c637e4e107
JohnCorby <williamforgames6@gmail.com>
2024-11-03 17:34:17 -0800

    work on admin

--- OWJam4ModProject/AdminArtifact.cs
+++ OWJam4ModProject/AdminArtifact.cs
@@ -1,11 +1,14 @@
 ﻿using NewHorizons.Builder.Props;
 using NewHorizons.External.Modules.Props;
+using OWML.Common;
 using UnityEngine;
 
 namespace OWJam4ModProject;
 
 public class AdminArtifact : MonoBehaviour
 {
+	public MorseCodeSensor[] CodeSensors;
+
 	public static void AttachToPlayerLantern()
 	{
 		var playerLantern = Locator.GetDreamWorldController().GetPlayerLantern();
@@ -16,5 +19,21 @@ public class AdminArtifact : MonoBehaviour
 			assetBundle = "assets/bundle",
 			path = "Assets/_Bundle/Prefabs/AdminArtifact.prefab"
 		});
+	}
+
+	private void Awake()
+	{
+		OWJam4ModProject.instance.ModHelper.Console.WriteLine("hello i am admin now hooray", MessageType.Success);
+
+		foreach (var sensor in CodeSensors)
+		{
+			sensor.OnEnterCode += () => OnEnterCode(sensor);
+			// yes this means we cant remove this event. tiny memory leak. oh well
+		}
+	}
+
+	private void OnEnterCode(MorseCodeSensor sensor)
+	{
+		OWJam4ModProject.instance.ModHelper.Console.WriteLine($"sensor {sensor} activate");
 	}
 }
