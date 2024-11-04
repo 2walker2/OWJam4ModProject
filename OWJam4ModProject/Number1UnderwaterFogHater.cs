13b5f02fe7b4a50f1ecd14e161754b2ba17043a0
JohnCorby <williamforgames6@gmail.com>
2024-11-03 21:26:59 -0800

    number 1 underwater fog hater

---
+++ OWJam4ModProject/Number1UnderwaterFogHater.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace OWJam4ModProject;
+
+/// <summary>
+/// turn off water renderer gameobjects so you dont have fog and stencil bullshit
+/// </summary>
+[RequireComponent(typeof(OWTriggerVolume))]
+public class Number1UnderwaterFogHater : MonoBehaviour
+{
+	private OWTriggerVolume _triggerVolume;
+	public GameObject[] GameObjectsToDisable;
+
+	private void Awake()
+	{
+		_triggerVolume = GetComponent<OWTriggerVolume>();
+
+		_triggerVolume.OnEntry += OnEntry;
+		_triggerVolume.OnExit += OnExit;
+	}
+
+	private void OnDestroy()
+	{
+		_triggerVolume.OnEntry -= OnEntry;
+		_triggerVolume.OnExit -= OnExit;
+	}
+
+	private void OnEntry(GameObject hitobj)
+	{
+		foreach (var go in GameObjectsToDisable) go.SetActive(false);
+	}
+
+	private void OnExit(GameObject hitobj)
+	{
+		foreach (var go in GameObjectsToDisable) go.SetActive(true);
+	}
+}
